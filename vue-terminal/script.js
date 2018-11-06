42b9b93b22f5bd30da85f4f54150da1c4232fa7c
artslob <artslob@yandex.ru>
2018-11-06 23:26:50 +0300

    basic layout and ternimal window

--- vue-terminal/script.js
+++ vue-terminal/script.js
@@ -1,6 +1,27 @@
+var demo = new Vue({
+    el: '#demo',
+    data: {
+        name: 'demo name'
+    }
+});
+
 var app = new Vue({
     el: '#app',
     data: {
         name: 'artyom',
-    }
+        demo: demo.name
+    },
+    methods: {
+        test: function () {
+            return new Date();
+        }
+    },
+    computed: {
+        reversed: function () {
+            return this.name.split('').reverse().join('');
+        }
+    },
+    mounted: function () {
+        console.log('hi');
+    },
 });
